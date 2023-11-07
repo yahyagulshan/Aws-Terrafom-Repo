8a285db020e2a85829c4d8ea23279d0b8c8e9570
yahyagulshan <m.yahya.gulshan@gmail.com>
2023-11-07 19:37:09 +0500

    add files

---
+++ backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "local" {
+
+		path = "./state/terraform.tfstate"
+	}
+}
+
+
+# terraform {
+#   backend "s3" {
+#     encrypt = true
+#     key     = "terraform.tfstate"
+#   }
+# }
+
+# terraform {
+#   backend "local" {}
+# }
