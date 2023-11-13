bd21ccc12b02c1ee98d9cd060bffc07ae92bce78
yahyagulshan <59036269+yahyagulshan@users.noreply.github.com>
2023-11-13 20:51:20 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,3 +4,15 @@ terraform {
 		path = "./state/terraform.tfstate"
 	}
 }
+
+########## state file on S3 ##################
+# terraform {
+#   backend "s3" {
+#     encrypt = true
+#     key     = "terraform.tfstate"
+#   }
+# }
+
+# terraform {
+#   backend "local" {}
+# }
