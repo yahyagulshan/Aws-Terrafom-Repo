0c573c433cd014ffc0ad00f15f72231da4576a2e
yahyagulshan <59036269+yahyagulshan@users.noreply.github.com>
2023-11-14 21:30:51 +0500

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -62,7 +62,7 @@ module "cdn" {
   ]
 
   viewer_certificate = {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:xxxxxxxxxxxxx:certificate/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxxx"
     ssl_support_method  = "sni-only"
   }
 }
